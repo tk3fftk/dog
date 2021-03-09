76bde85bf55c9c455eb9ca693cb51bb714b334b0
Hiroki Takatsuka <tktk.stereoman@gmail.com>
2021-03-09 23:52:47 +0900

    refactor

--- app/src/main/java/com/example/androiddevchallenge/TimerScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/TimerScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material.TextField
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,6 +34,20 @@ fun TimerScreen(timerViewModel: TimerViewModel) {
                     timerValue = timerValue,
                     timerFinished = timerFinished
                 )
+                /*
+                TimerFieldWithInput(
+                    timerValue = timerValue,
+                    timerStarted = timerStarted,
+                    updateTimer = updateTimer,
+                    timerFinished = timerFinished,
+                    onImeAction = startTimer,
+                )
+                 */
+                TimerButtons(
+                    stopTimer = stopTimer,
+                    updateTimer = updateTimer,
+                    timerStarted = timerStarted,
+                )
                 if (!timerStarted) {
                     TimerInput(
                         modifier = Modifier,
@@ -44,11 +57,6 @@ fun TimerScreen(timerViewModel: TimerViewModel) {
                         startTimer = startTimer,
                     )
                 }
-                TimerButtons(
-                    stopTimer = stopTimer,
-                    updateTimer = updateTimer,
-                    timerStarted = timerStarted,
-                )
             }
         }
     }
@@ -120,17 +128,6 @@ private fun TimerInput(
             updateTimer = updateTimer,
             onImeAction = startTimer
         )
-        /*
-        Button(
-            onClick = {
-                startTimer()
-            },
-            modifier = modifier.align(Alignment.CenterVertically),
-            enabled = !timerStarted
-        ) {
-            Text("Start")
-        }
-         */
     }
 }
 
@@ -146,10 +143,11 @@ fun TimerInputField(
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
     TextField(
-        modifier = modifier,
+        modifier = modifier.padding(32.dp),
         value = timerValue.toString(),
         onValueChange = updateTimer,
         colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
+        label = { Text("input time here!") },
         maxLines = 1,
         enabled = !timerStarted,
         keyboardOptions = KeyboardOptions.Default.copy(
@@ -177,7 +175,8 @@ private fun TimerField(modifier: Modifier = Modifier, timerValue: Int, timerFini
         Text(
             modifier = modifier.fillMaxWidth(),
             textAlign = TextAlign.Center,
-            text = "finished"
+            fontSize = 32.sp,
+            text = "finished\uD83D\uDE80"
         )
     }
 }
@@ -188,18 +187,46 @@ fun toTimerString(value: Int): String {
     return "${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}"
 }
 
-/*
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun LightPreview() {
-    TimerInput(
-        timerValue = 0,
-        timerStarted = false,
-        updateTimer = { /*TODO*/ },
-        startTimer = { /*TODO*/ }
+private fun TimerFieldWithInput(
+    modifier: Modifier = Modifier,
+    timerValue: Int,
+    timerStarted: Boolean,
+    updateTimer: (String) -> Unit,
+    onImeAction: () -> Unit = {},
+    timerFinished: Boolean
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    TextField(
+        value = toTimerString(timerValue),
+        onValueChange = updateTimer,
+        modifier = modifier
+            .padding(top = 150.dp)
+            .fillMaxWidth(),
+        enabled = !timerStarted,
+        textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center, fontSize = 128.sp),
+        colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
+        maxLines = 1,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Number
+        ),
+        keyboardActions = KeyboardActions(onDone = {
+            onImeAction()
+            keyboardController?.hideSoftwareKeyboard()
+        })
+
     )
+    if (timerFinished) {
+        Text(
+            modifier = modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontSize = 32.sp,
+            text = "finished\uD83D\uDE80"
+        )
+    }
 }
-*/
 
 @Preview
 @Composable
